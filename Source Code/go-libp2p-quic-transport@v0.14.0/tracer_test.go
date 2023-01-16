700b61975379050408080af116305c24513f9a9b
vincennt99v1123li <49277518+vincennt99v1123li@users.noreply.github.com>
2023-01-16 20:28:10 +0800

    Add files via upload

---
+++ Source Code/go-libp2p-quic-transport@v0.14.0/tracer_test.go
@@ -0,0 +1,82 @@
+package libp2pquic
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/klauspost/compress/zstd"
+
+	"github.com/lucas-clemente/quic-go/logging"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("qlogger", func() {
+	var qlogDir string
+
+	BeforeEach(func() {
+		var err error
+		qlogDir, err = ioutil.TempDir("", "libp2p-quic-transport-test")
+		Expect(err).ToNot(HaveOccurred())
+		fmt.Fprintf(GinkgoWriter, "Creating temporary directory: %s\n", qlogDir)
+		initQlogger(qlogDir)
+	})
+
+	AfterEach(func() {
+		Expect(os.RemoveAll(qlogDir)).To(Succeed())
+	})
+
+	getFile := func() os.FileInfo {
+		files, err := ioutil.ReadDir(qlogDir)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(files).To(HaveLen(1))
+		return files[0]
+	}
+
+	It("saves a qlog", func() {
+		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte{0xde, 0xad, 0xbe, 0xef})
+		file := getFile()
+		Expect(string(file.Name()[0])).To(Equal("."))
+		Expect(file.Name()).To(HaveSuffix(".qlog.swp"))
+		// close the logger. This should move the file.
+		Expect(logger.Close()).To(Succeed())
+		file = getFile()
+		Expect(string(file.Name()[0])).ToNot(Equal("."))
+		Expect(file.Name()).To(HaveSuffix(".qlog.zst"))
+		Expect(file.Name()).To(And(
+			ContainSubstring("server"),
+			ContainSubstring("deadbeef"),
+		))
+	})
+
+	It("buffers", func() {
+		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
+		initialSize := getFile().Size()
+		// Do a small write.
+		// Since the writter is buffered, this should not be written to disk yet.
+		logger.Write([]byte("foobar"))
+		Expect(getFile().Size()).To(Equal(initialSize))
+		// Close the logger. This should flush the buffer to disk.
+		Expect(logger.Close()).To(Succeed())
+		finalSize := getFile().Size()
+		fmt.Fprintf(GinkgoWriter, "initial log file size: %d, final log file size: %d\n", initialSize, finalSize)
+		Expect(finalSize).To(BeNumerically(">", initialSize))
+	})
+
+	It("compresses", func() {
+		logger := newQlogger(qlogDir, logging.PerspectiveServer, []byte("connid"))
+		logger.Write([]byte("foobar"))
+		Expect(logger.Close()).To(Succeed())
+		compressed, err := ioutil.ReadFile(qlogDir + "/" + getFile().Name())
+		Expect(err).ToNot(HaveOccurred())
+		Expect(compressed).ToNot(Equal("foobar"))
+		c, err := zstd.NewReader(bytes.NewReader(compressed))
+		Expect(err).ToNot(HaveOccurred())
+		data, err := ioutil.ReadAll(c)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(data).To(Equal([]byte("foobar")))
+	})
+})
---
+++ Source Code/go-libp2p-quic-transport@v0.14.0/transport_test.go
@@ -0,0 +1,77 @@
+package libp2pquic
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io"
+	"net"
+
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	tpt "github.com/libp2p/go-libp2p-core/transport"
+	"github.com/lucas-clemente/quic-go"
+	ma "github.com/multiformats/go-multiaddr"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Transport", func() {
+	var t tpt.Transport
+
+	BeforeEach(func() {
+		rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		Expect(err).ToNot(HaveOccurred())
+		key, err := ic.UnmarshalRsaPrivateKey(x509.MarshalPKCS1PrivateKey(rsaKey))
+		Expect(err).ToNot(HaveOccurred())
+		t, err = NewTransport(key, nil, nil)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		Expect(t.(io.Closer).Close()).To(Succeed())
+	})
+
+	It("says if it can dial an address", func() {
+		invalidAddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1234")
+		Expect(err).ToNot(HaveOccurred())
+		validAddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1234/quic")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(t.CanDial(invalidAddr)).To(BeFalse())
+		Expect(t.CanDial(validAddr)).To(BeTrue())
+	})
+
+	It("says that it cannot dial /dns addresses", func() {
+		addr, err := ma.NewMultiaddr("/dns/google.com/udp/443/quic")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(t.CanDial(addr)).To(BeFalse())
+	})
+
+	It("supports the QUIC protocol", func() {
+		protocols := t.Protocols()
+		Expect(protocols).To(HaveLen(1))
+		Expect(protocols[0]).To(Equal(ma.P_QUIC))
+	})
+
+	It("uses a conn that can interface assert to a UDPConn for dialing", func() {
+		origQuicDialContext := quicDialContext
+		defer func() { quicDialContext = origQuicDialContext }()
+
+		var conn net.PacketConn
+		quicDialContext = func(_ context.Context, c net.PacketConn, _ net.Addr, _ string, _ *tls.Config, _ *quic.Config) (quic.Session, error) {
+			conn = c
+			return nil, errors.New("listen error")
+		}
+		remoteAddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/0/quic")
+		Expect(err).ToNot(HaveOccurred())
+		_, err = t.Dial(context.Background(), remoteAddr, "remote peer id")
+		Expect(err).To(MatchError("listen error"))
+		Expect(conn).ToNot(BeNil())
+		defer conn.Close()
+		_, ok := conn.(udpConn)
+		Expect(ok).To(BeTrue())
+	})
+})
